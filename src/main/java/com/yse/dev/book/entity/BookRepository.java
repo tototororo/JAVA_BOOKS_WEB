8c2c1480905abaada6f6a5da7b70ded3ef24d8c0
tototororo <76872204+tototororo@users.noreply.github.com>
2023-09-26 15:23:07 +0900

    Update BookRepository.java
    검색 기능 추가

--- src/main/java/com/yse/dev/book/entity/BookRepository.java
+++ src/main/java/com/yse/dev/book/entity/BookRepository.java
@@ -1,7 +1,12 @@
 package com.yse.dev.book.entity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-	
+	//제목 검색 기능 추가
+	// findBy{멤버변수명} (멤버변수타입 매개변수)
+	//Contains 붙으면 포함처리 예: Like '%스프링%' 
+    Page<Book> findByTitleContains(String title, Pageable pageable);
 }
